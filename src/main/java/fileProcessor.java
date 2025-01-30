a2a8a0cc9811ebd803eb4dcfbca4f335077d3aa5
jonathan.mileham <jonathan.mileham@JonathanM-LT.tbafrica.local>
2025-01-30 07:18:37 +0200

    Change in file structure

---
+++ src/main/java/fileProcessor.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author jonathan.mileham
+ */
+@WebServlet(urlPatterns = {"/fileProcessor"})
+@MultipartConfig
+public class fileProcessor extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        getFiles(request,response);
+    }
+    
+    private void getFiles(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{ 
+        String line;
+        PrintWriter out = response.getWriter();
+
+            for (Part part : request.getParts()) {
+                BufferedReader bufReader = new BufferedReader(new InputStreamReader(part.getInputStream()));
+                
+                while ((line=bufReader.readLine())!=null) {
+                    
+                    out.print(line+"\n");
+                    
+                }
+                
+                bufReader.close();
+            }
+        
+       
+        
+    }
+
+
+}
