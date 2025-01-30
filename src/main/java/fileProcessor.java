66957a0620b0db12447354b889574e906d1c3cc8
Wolfdecill <jontymileham@gmail.com>
2025-01-30 13:44:29 +0200

    Fully Functional Code

--- src/main/java/fileProcessor.java
+++ src/main/java/fileProcessor.java
@@ -14,6 +14,9 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,31 +27,78 @@ import java.io.InputStreamReader;
 public class fileProcessor extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
         
-        getFiles(request,response);
+        try {
+            getFiles(request,response);
+        } catch (IOException ex) {
+            System.out.println("Error accessing the response");
+        }
     }
     
-    private void getFiles(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{ 
-        String line;
+    private void getFiles(HttpServletRequest request, HttpServletResponse response) throws IOException{      
+        String delimiter;
+        BufferedReader bufReader = null;
         PrintWriter out = response.getWriter();
-
-            for (Part part : request.getParts()) {
-                BufferedReader bufReader = new BufferedReader(new InputStreamReader(part.getInputStream()));
-                
-                while ((line=bufReader.readLine())!=null) {
-                    
-                    out.print(line+"\n");
-                    
-                }
-                
-                bufReader.close();
-            }
+        int gp,sp,dental,optom,path,aux,radio;
         
-       
+        try {     
+            delimiter= request.getParameter("delimiter");
+            out.print("Delimiter used: "+delimiter+"\n");
+            //Each "part" is a different file
+            for (Part part : request.getParts()) {
+                
+                if(part.getSubmittedFileName()!=null){
+                    //File Name
+                    out.print("\n"+part.getSubmittedFileName()+"\n");
+                
+                    //Convert "part" to someting readable
+                    bufReader = new BufferedReader(new InputStreamReader(part.getInputStream()));
+                    ArrayList<String> convertedBuffer= convertBufferedReaderToArrayList(bufReader);
+                    
+                    //Get how many occurances of a search their is
+                    gp= searchResults(convertedBuffer,delimiter,"GP");
+                    sp= searchResults(convertedBuffer,delimiter,"Specialists");
+                    dental= searchResults(convertedBuffer,delimiter,"Dental");
+                    optom= searchResults(convertedBuffer,delimiter,"Optom");
+                    path= searchResults(convertedBuffer,delimiter,"Pathology");
+                    aux= searchResults(convertedBuffer,delimiter,"Auxiliary");
+                    radio= searchResults(convertedBuffer,delimiter,"Radiology");
+
+                    out.print("GP:"+gp+" SP:"+sp+" Dental:"+dental+" Optom:"+optom+" Path:"+path+" Aux:"+aux+" Radio:"+radio+"\n");
+                }
+            }    
+        }
+        catch (IOException e) {out.print("Error occured while trying to read file \n");} 
+        catch (ServletException ex) {out.print("Error occured while trying to get file \n");}
+            
+            
         
     }
+    
+    private int searchResults(ArrayList<String> convertedBuffer ,String delimiter, String search){
+        int count=0;
+        int loop=0;
+        String line; 
+        //Change this to change what it is searching for
+        String contains= delimiter+search;
+        
+        while(loop<=convertedBuffer.size()-1){
+            if(convertedBuffer.get(loop).contains(contains)){
+                count++;
+            }
+            loop++;
+        }
+        return count;
+    }
 
+    private ArrayList<String> convertBufferedReaderToArrayList(BufferedReader reader) throws IOException{
+        String line;
+        ArrayList<String> lines=new ArrayList<>();
+            while ( (line=reader.readLine())!=null){
+                lines.add(line);
+            }
+            return lines;
+        }
 
 }
